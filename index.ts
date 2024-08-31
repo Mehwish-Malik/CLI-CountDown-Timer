85c21e029b48cee7b512139167ee89e69a844743
wish Malik <malik.mehwish0078@gmail.com>
2024-09-01 02:00:09 +0500

    A Cli Based CountDown Timer for 15 Seconds

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+// Step 1
+import { DifferenceInSecondsOptions } from "date-fns";
+import { differenceInSeconds } from "date-fns/differenceInSeconds";
+function* countDownTimer(second:number){
+    while(second > 0){
+    yield second;
+    second--;
+}
+}
+// step 2(Initialization of counter)
+let timerIterator = countDownTimer(15)
+// function to create count down timer
+function displayCountDown(){
+    // value below 10
+    let result = timerIterator.next();
+    if(!result.done){
+        const now = new Date();
+    //    calculate minutes in time
+        const countDownTime = new Date(now.getTime() + (result.value *1000))
+    // Calculate remaining seconds in time
+    const remainingSeconds = differenceInSeconds(countDownTime , now)
+    console.log(`Remaining Seconds:${remainingSeconds}`);
+    
+    setTimeout(displayCountDown,1000)
+    }else{
+        // display result countDown Complete
+        console.log("CountDown Completed!!");
+    }
+}
+displayCountDown()
